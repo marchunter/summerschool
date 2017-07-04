0061fedb0ef40f73e80d809278ff1caae757739b
marchunter <marc.o.j.jaeger@gmail.com>
2017-07-04 15:15:13 +0300

    hybrid hello finished.

--- exercises/hybrid_programming/c/hello.c
+++ exercises/hybrid_programming/c/hello.c
@@ -1,16 +1,22 @@
 // Hello world with mpi and openmp
 
-void main(int *argc, char argv[]) {
+#include <omp.h>
+#include <mpi.h>
+#include <stdio.h>
 
-	MPI_Init();
-	MPI_Comm_rank(MPI_COMM_WORLD);
+void main(int argc, char *argv[]) {
 
-#pragma omp parallel
-	int id;	
-	id = get_omp_thread_num();
+	int rank;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+#pragma omp parallel 
+	{	
+	int thread;	
+	thread = omp_get_thread_num();
 
 	printf("Hello, world - signed by process %d, thread %d.\n", rank, thread);
-
+	}
 
 	MPI_Finalize();
 
